d838e0995195df24504bb7d980b64b13e40e07e0
Asif Yousaf <justforexample13@gmail.com>
2026-01-17 14:34:58 +0400

    1

--- api/flights/index.js
+++
@@ -1,5 +0,0 @@
-// Redirect /api/flights to /api/flights/offers
-export default async function handler(req, res) {
-  const query = new URLSearchParams(req.query).toString();
-  res.redirect(307, `/api/flights/offers?${query}`);
-}
